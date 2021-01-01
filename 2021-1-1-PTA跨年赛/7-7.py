5cd63b84fa0838919860d44e9d5cc87d4ab10f37
Hacker-C <1507559148@qq.com>
2021-01-01 21:39:49 +0800

    2020PTA跨年赛

---
+++ 2021-1-1-PTA跨年赛/7-7.py
@@ -0,0 +1,10 @@
+n = int(input())
+for i in range(n):
+    s = input()
+    if s[-1]=='?':
+        if 'PTA' in s:
+            print('Yes!')
+        else:
+            print('No.')
+    else:
+        print('enen')
