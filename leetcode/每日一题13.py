0902f328cf7db57d9abc15ee8322c1e57459c1ef
Hacker-C <1507559148@qq.com>
2021-01-23 11:25:45 +0800

    完成力扣每日一题#9

--- leetcode/每日一题13.py
+++ leetcode/每日一题13.py
@@ -1,20 +1,5 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        base = {
-            'I':1,
-            'V':5,
-            'X':10,
-            'L':50,
-            'C':100,
-            'D':500,
-            'M':1000,
-            'IV':4,
-            'IX':9,
-            'XL':40,
-            'XC':90,
-            'CD':400,
-            'CM':900
-        }
         temp={
             'a':1,
             'b':5,
@@ -30,8 +15,8 @@ class Solution:
             'l':400,
             'm':900
         }
-        baseKeys=list(base.keys())
-        tempKeys=list(temp.keys())
+        baseKeys=['I','V','X','L','C','D','M','IV','IX','XL','XC','CD','CM']
+        tempKeys=['a','b','c','d','e','f','g','h','i','j','k','l','m']
         ans=0
         for i in range(12, -1, -1):
             s=s.replace(baseKeys[i], tempKeys[i])
@@ -39,7 +24,3 @@ class Solution:
             ans+=temp[i]
         return ans
 
-if __name__ == '__main__':
-    a = Solution()
-    palindrome_bool = a.romanToInt("III")
-    print(palindrome_bool)  
