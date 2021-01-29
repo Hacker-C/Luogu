af31408a2b0ab76059848804c77f4769c4a8bf41
Hacker-C <1507559148@qq.com>
2021-01-29 14:18:17 +0800

    优化目录结构

--- leetcode/35.py
+++ leetcode/35.py
@@ -5,13 +5,10 @@ class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         nums.append('')
         l=len(nums)
-        flag=1
         for i in range(l-1):
             if target<=nums[i]:
                 for j in range(l-2, i-1, -1):
                     nums[j+1]=nums[j]
                 nums[i]=target
-                flag=0
                 return i
-        if flag:
-            return l-1
+        return l-1
