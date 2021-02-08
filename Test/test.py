fad5bbedfd3ab05305e3ebb3d3bd6c01e414749c
Hacker-C <1507559148@qq.com>
2021-02-08 16:32:55 +0800

    just a test

--- Test/test.py
+++ Test/test.py
@@ -1,5 +1,42 @@
-from functools import reduce
+from itertools import chain
 
-nums = [1, 2, 3, 4, 5]
-sm = reduce(lambda x, y: x + y, nums)
-print(sm)
+
+def spread(ls):
+    ans = []
+    for i in ls:
+        if isinstance(i, list):
+            return spread(i)
+        else:
+            ans.append(i)
+    return ans
+
+
+def spread(lst, res=None):
+    if res is None:
+        res = []
+    for item in lst:
+        if isinstance(item, list):
+            spread(item, res)
+        else:
+            res.append(item)
+    return res
+
+
+def spred2(lst):
+    res = []
+    while lst:
+        head = lst.pop(0)
+        if isinstance(head, list):
+            lst = head + lst
+        else:
+            res.append(head)
+    return res
+
+
+inputlist = ['it', 'is', ['a', ['test', 'of', ['circle', 'lists'], ','], 'please', 'like', ['it', 'and'], 'hello'], 'world']
+
+
+lst = [1, [2, 3], [[4, [5, 6]], 7], [8, 9], 10]
+print(spread(lst))
+# print(spread(lst))
+print(flat2(inputlist))
