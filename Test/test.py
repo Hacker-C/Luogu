12727635a3436884146125b41cbecab56a362899
Hacker-C <1507559148@qq.com>
2021-02-17 15:28:23 +0800

    杨辉三角取值&回文字符串判断的递归实现

--- Test/test.py
+++ Test/test.py
@@ -1,42 +1,9 @@
-from itertools import chain
+def getValue(x, y):
+    if 0 <= y <= x:
+        if y == 0 or x == y:
+            return 1
+        return getValue(x - 1, y - 1) + getValue(x - 1, y)
+    return -1
 
 
-def spread(ls):
-    ans = []
-    for i in ls:
-        if isinstance(i, list):
-            return spread(i)
-        else:
-            ans.append(i)
-    return ans
-
-
-def spread(lst, res=None):
-    if res is None:
-        res = []
-    for item in lst:
-        if isinstance(item, list):
-            spread(item, res)
-        else:
-            res.append(item)
-    return res
-
-
-def spred2(lst):
-    res = []
-    while lst:
-        head = lst.pop(0)
-        if isinstance(head, list):
-            lst = head + lst
-        else:
-            res.append(head)
-    return res
-
-
-inputlist = ['it', 'is', ['a', ['test', 'of', ['circle', 'lists'], ','], 'please', 'like', ['it', 'and'], 'hello'], 'world']
-
-
-lst = [1, [2, 3], [[4, [5, 6]], 7], [8, 9], 10]
-print(spread(lst))
-# print(spread(lst))
-print(flat2(inputlist))
+print(getValue(3, 2))
